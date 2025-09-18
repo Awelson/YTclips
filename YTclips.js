c8b7797d7342c1ec73d4b0f9709959af787fb4aa
Awelson <aaronwelson@gmail.com>
2025-09-18 15:18:57 +0100

    removed some redundant code in YTclips.js

--- YTclips.js
+++ YTclips.js
@@ -8,37 +8,13 @@ var player;
 
 // 2. This function will be called by the API once it's loaded.
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
+    player = new YT.Player('player', {});
 }
 
-// 3. This function will be called when the player is ready.
-function onPlayerReady(event) {
-    // Get all elements with the class 'clickable-segment'
-    var clickableElements = document.querySelectorAll('.clickable-segment');
-
-    // Add a click event listener to each clickable element
-    clickableElements.forEach(function(element) {
-        element.addEventListener('click', function() {
-            // Get the start and end times from the data attributes
-            var startTime = this.getAttribute('data-start-time');
-            var endTime = this.getAttribute('data-end-time');
-
-            // Check if the times are valid numbers before playing
-            if (!isNaN(startTime) && !isNaN(endTime)) {
-                playSegment(parseFloat(startTime), parseFloat(endTime));
-            }
-        });
-    });
-}
-
-// 4. A variable to hold the interval for checking the video time.
+// 3. A variable to hold the interval for checking the video time.
 var timeInterval;
 
-// 5. Function to play a specific segment
+// 4. Function to play a specific segment
 function playSegment(startTime, endTime) {
     clearInterval(timeInterval);
 
